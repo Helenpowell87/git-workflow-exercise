b7f3237a04303a85f33e240c36498d1b2aaa30c1
Helen <helenpowell@hotmail.com>
2023-06-13 11:06:34 +0100

    changes to flow

--- flow.sh
+++ flow.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # stage changes
-git add -A
+git add --all
 
 # commit changes
 git commit -m "changes to flow"
 
 # push changes
-git push -u origin Helen
+git push -u origin Helenpowell87
